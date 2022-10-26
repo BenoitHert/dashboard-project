ef35462be25292a11b0a4aebc719dc76418fd059
salted-brocco-lee <benoit.hert@hotmail.fr>
2022-10-26 09:08:02 +0200

    Commit

---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+// components
+
+export {default as Button} from './Button';
+export {default as Cart} from './Cart';
+export {default as Footer} from './Footer';
+export {default as Header} from './Header';
+export {default as Navbar} from './Navbar';
+export {default as Sidebar} from './Sidebar';
+export {default as ThemeSettings} from './ThemeSettings';
+export {default as UserProfile} from './UserProfile';
+export {default as Notification } from './Notification';
---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+// apps
+
+export {default as TodoList} from './TodoList';
+export {default as Agenda} from './Agenda';
+export {default as Calendar} from './Calendar';
+export {default as ShoppingList} from './ShoppingList';
+export {default as Kanban} from './Kanban';
+
+
+// Dashboard
+
+export {default as Dashboard} from './Dashboard';
+
+
+// Pages
+
+export {default as AboutPage} from './AboutPage';
+export {default as Menus} from './Menus';
